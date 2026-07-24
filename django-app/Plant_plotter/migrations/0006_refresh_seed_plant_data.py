bbb2911e7c366543bd0f44add7fda7b0bcd0dccd
Jessica <jn00748@surrey.ac.uk>
2026-07-24 02:03:30 +0100

    update

--- django-app/Plant_plotter/migrations/0006_refresh_seed_plant_data.py
+++ django-app/Plant_plotter/migrations/0006_refresh_seed_plant_data.py
@@ -29,12 +29,6 @@ PLANT_FIELD_DEFAULTS = {
 
 def refresh_seed_data(apps, schema_editor):
     plant_model = apps.get_model("Plant_plotter", "Plant")
-    helps_model = apps.get_model("Plant_plotter", "Companion_helpslistItem")
-    helped_by_model = apps.get_model(
-        "Plant_plotter",
-        "Companion_helped_bylistItem",
-    )
-    avoid_model = apps.get_model("Plant_plotter", "Plants_avoidlistItem")
 
     sample_path = (
         Path(__file__).resolve().parents[1]
@@ -44,40 +38,15 @@ def refresh_seed_data(apps, schema_editor):
     )
     sample_plants = json.loads(sample_path.read_text(encoding="utf-8"))["plants"]
 
-    plants_by_name = {}
     for data in sample_plants:
         defaults = {
             field: data.get(field, default)
             for field, default in PLANT_FIELD_DEFAULTS.items()
         }
-        plant, _ = plant_model.objects.update_or_create(
+        plant_model.objects.update_or_create(
             name=data["name"],
             defaults=defaults,
         )
-        plants_by_name[plant.name] = plant
-
-    seed_ids = [plant.id for plant in plants_by_name.values()]
-    helps_model.objects.filter(plant_id__in=seed_ids).delete()
-    helped_by_model.objects.filter(plant_id__in=seed_ids).delete()
-    avoid_model.objects.filter(plant_id__in=seed_ids).delete()
-
-    for data in sample_plants:
-        plant = plants_by_name[data["name"]]
-        for other_name in data.get("companion_helps", []):
-            helps_model.objects.get_or_create(
-                plant=plant,
-                other_plant=plants_by_name[other_name],
-            )
-        for other_name in data.get("companion_helped_by", []):
-            helped_by_model.objects.get_or_create(
-                plant=plant,
-                other_plant=plants_by_name[other_name],
-            )
-        for other_name in data.get("plants_avoid", []):
-            avoid_model.objects.get_or_create(
-                plant=plant,
-                other_plant=plants_by_name[other_name],
-            )
 
 
 class Migration(migrations.Migration):
